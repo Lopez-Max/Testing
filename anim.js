e17b52a4ee195b615333eeb3880d02030145f23a
Lopez-Max <maxlopezAT@gmail.com>
2025-02-08 10:30:07 -0600

    Update anim.js

--- anim.js
+++ anim.js
@@ -6,7 +6,7 @@ var lyrics = document.querySelector("#lyrics");
 var lyricsData = [
   { text: "Your morning eyes", time: 18-3-8 },
   { text: "I could stare like watching stars", time: 19.5-3-10 },
-  { text: "I could walk you be,", time: 23-3-7 },
+  { text: "I could walk you by,", time: 23-3-7 },
   { text: "and I'll tell without a thought", time: 27-3-10 },
   { text: "You'd be mine, would you mind", time: 31-3-10 },
   { text: "if I took your hand tonight?", time: 33-3-10-1 },
