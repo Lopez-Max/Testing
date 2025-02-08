4013a57f39208ba59b98b76219a6446f544de47a
Lopez-Max <maxlopezAT@gmail.com>
2025-02-08 11:19:09 -0600

    Update anim.js

--- anim.js
+++ anim.js
@@ -4,10 +4,10 @@ var lyrics = document.querySelector("#lyrics");
 
 // Array de objetos que contiene cada línea y su tiempo de aparición en segundos
 var lyricsData = [
-  { text: "Your morning eyes", time: 4 },
+  { text: "Your morning eyes", time: 5 },
   { text: "I could stare like watching stars", time: 6 },
   { text: "I could walk you by,", time: 23-3-7 },
-  { text: "and I'll tell without a thought", time: 27-3-12 },
+  { text: "and I'll tell without a thought", time: 27-3-13 },
   { text: "You'd be mine, would you mind", time: 31-3-10 },
   { text: "if I took your hand tonight?", time: 33-3-10-1 },
   { text: "Know you're all that I want this life", time: 38-3-10 },
